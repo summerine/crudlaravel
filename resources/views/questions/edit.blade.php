daa6a29a22954df92cc4fdccaa5b3148c391d91a
summerine <indri.pr.26@gmail.com>
2020-07-04 15:36:11 +0700

    editing questions

---
+++ resources/views/questions/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('questions.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Questions</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('questions.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('questions.update',$question->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Title:</strong>
+                    <input type="text" name="question" value="{{ $question->name }}" class="form-control" placeholder="Question">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Content:</strong>
+                    <textarea class="form-control" style="height:150px" name="content" placeholder="content">{{ $question->content }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>User ID</strong>
+                    <input type="text" name="user_id" value="{{ $question->user_id }}" class="form-control" readonly>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,54 @@
+@extends('questions.layout')
+ 
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Larahub</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('questions.create') }}"> Create New Questions</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Content</th>
+            <th>User ID</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($questions as $question)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $question->title }}</td>
+            <td>{{ $question->content }}</td>
+            <td>{{ $question->user_id }}</td>
+            <td>
+                <form action="{{ route('questions.destroy',$question->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('questions.show',$question->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('questions.edit',$question->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $questions->links() !!}
+      
+@endsection
